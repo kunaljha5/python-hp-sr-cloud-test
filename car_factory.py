3a40bcb8d391ac0a55230e4ad1802441fec67647
Kunal Jha <kunaljha5@gmail.com>
2023-06-20 00:07:51 +0530

    Changes for first flow

---
+++ car_factory.py
@@ -0,0 +1,89 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+@dataclass
+class EngineCylinders:
+    value: int
+
+
+@dataclass
+class Wheels(Enum):
+    FOUR = 4
+    TWO = 2
+
+
+@dataclass
+class Doors(Enum):
+    TWO = 2
+    FOUR = 4
+    ZERO = 0
+
+
+def validate_configuration(configuration):
+    vehicle_type = configuration[0]
+    params = configuration[1:]
+
+    if vehicle_type == "Truck":
+        return validate_truck_configuration(params)
+    elif vehicle_type == "Car":
+        return validate_car_configuration(params)
+    elif vehicle_type == "Scooter":
+        return validate_scooter_configuration(params)
+    else:
+        raise ValueError("Invalid vehicle type")
+
+
+def validate_truck_configuration(params):
+    engine_cylinders, wheels, doors = params
+
+    if not isinstance(
+        engine_cylinders, EngineCylinders
+    ) or engine_cylinders.value not in [4, 6, 8]:
+        return False
+
+    if wheels is not Wheels.FOUR:
+        return False
+
+    if doors is not Doors.FOUR:
+        return False
+
+    return True
+
+
+def validate_car_configuration(params):
+    engine_cylinders, wheels, doors = params
+
+    if not isinstance(
+        engine_cylinders, EngineCylinders
+    ) or engine_cylinders.value not in [4, 6]:
+        return False
+
+    if wheels is not Wheels.FOUR:
+        return False
+
+    if doors not in [Doors.TWO, Doors.FOUR]:
+        return False
+
+    return True
+
+
+def validate_scooter_configuration(params):
+    """
+    :param params:
+    :return: boolean
+    """
+    engine_cylinders, wheels, doors = params
+
+    if not isinstance(
+        engine_cylinders, EngineCylinders
+    ) or engine_cylinders.value not in [1, 2, 3]:
+        return False
+
+    if wheels is not Wheels.TWO:
+        return False
+
+    if doors is not Doors.ZERO:
+        return False
+
+    return True
---
+++ test.py
@@ -0,0 +1,46 @@
+from car_factory import (
+    EngineCylinders,
+    Wheels,
+    Doors,
+    validate_configuration,
+)
+
+
+def test_valid_truck_configuration():
+    configuration = ["Truck", EngineCylinders(6), Wheels.FOUR, Doors.FOUR]
+    assert validate_configuration(configuration) is True
+
+
+def test_invalid_truck_configuration():
+    configuration = ["Truck", EngineCylinders(1), Wheels.FOUR, Doors.FOUR]
+    assert validate_configuration(configuration) is False
+
+
+def test_valid_car_configuration():
+    configuration = ["Car", EngineCylinders(4), Wheels.FOUR, Doors.TWO]
+    assert validate_configuration(configuration) is True
+
+
+def test_invalid_car_configuration():
+    configuration = ["Car", EngineCylinders(8), Wheels.FOUR, Doors.ZERO]
+    assert validate_configuration(configuration) is False
+
+
+def test_valid_scooter_configuration():
+    configuration = ["Scooter", EngineCylinders(1), Wheels.TWO, Doors.ZERO]
+    assert validate_configuration(configuration) is True
+
+
+def test_invalid_scooter_configuration():
+    configuration = ["Scooter", EngineCylinders(3), Wheels.TWO, Doors.FOUR]
+    assert validate_configuration(configuration) is False
+
+
+def test_valid_motorcycle_configuration():
+    configuration = ["Motorcycle", EngineCylinders(4), Wheels.ONE, Doors.ZERO]
+    assert validate_configuration(configuration) is True
+
+
+def test_invalid_motorcycle_configuration():
+    configuration = ["Motorcycle", EngineCylinders(6), Wheels.ONE, Doors.TWO]
+    assert validate_configuration(configuration) is False
